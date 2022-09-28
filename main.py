04bce967e463b19fbcae191c496a95446c4e5252
Nat-1501 <natalisoares90@gmail.com>
2022-09-28 20:46:41 -0300

    criado frames telas

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+
+cor1 = "#3b3b3b"
+cor2 = "#feffff"
+cor3 = "#38576b"
+cor4 = "#ECEFF1"
+cor5 = "#FFAB40"
+
+
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+janela.mainloop()
