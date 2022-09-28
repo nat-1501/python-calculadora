3b54544c54bf331309dc6e21bc432d55fd6c2a7e
Nat-1501 <natalisoares90@gmail.com>
2022-09-28 19:51:53 -0300

    teste

---
+++ aula 1/app.py
@@ -0,0 +1,10 @@
+nome = "Natali"
+idade = "23"
+gosto_de = "programação"
+
+
+print(f"Meu nome é {nome}.")
+print(f"Eu gosto de {gosto_de} ")
+print(f"Sou a {nome} e tenho {idade} e gosto {gosto_de} ")
+print(f"Nat gosta de programação e tem {idade} anos")
+print(f"Eu tenho {idade} ")
